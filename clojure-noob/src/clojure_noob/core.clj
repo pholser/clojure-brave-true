3d575efcf232cebc486d7dfcc99f0588948d1faf
Paul Holser <holser@railroad19.com>
2019-12-20 15:58:13 -0600

    Prepare to tackle Project Euler problems

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,11 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn train
+  []
+  (println "Choo choo!"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm a little teapot!"))
