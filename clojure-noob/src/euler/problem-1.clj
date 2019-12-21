474c716bf0caa5b2ba0b7c08b876dd9b3eec6bfa
Paul Holser <pholser@alumni.rice.edu>
2019-12-20 19:57:11 -0600

    problem 1

--- clojure-noob/src/euler/problem-1.clj
+++ clojure-noob/src/euler/problem-1.clj
@@ -1,1 +1,13 @@
 (ns clojure-noob.euler)
+
+(defn multiple-of? [divisor n]
+  (= 0 (mod n divisor)))
+
+(def multiple-of-3? (partial multiple-of? 3))
+(def multiple-of-5? (partial multiple-of? 5))
+
+(defn problem-1 []
+  (reduce +
+    (filter
+      (fn [n] (or (multiple-of-3? n) (multiple-of-5? n)))
+      (range 1000))))
